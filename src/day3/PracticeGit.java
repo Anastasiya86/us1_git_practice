dac8501ec00fa635a86a9c3d0433f92b948d035a
anastasiya zlatanova <anastasiya8611@gmail.com>
2022-11-05 04:26:42 -0500

    class practice is completed

--- src/day3/PracticeGit.java
+++ src/day3/PracticeGit.java
@@ -4,5 +4,6 @@ public class PracticeGit {
     public static void main(String[] args) {
         System.out.println("more practice");
         System.out.println("wip");
+        System.out.println("completed");
     }
 }

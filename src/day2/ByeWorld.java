bc0617ad1eacdbd5d8cdcd4de9d16ea145c464d2
anastasiya zlatanova <anastasiya8611@gmail.com>
2022-11-05 04:10:42 -0500

    ByeWorld class updated

--- src/day2/ByeWorld.java
+++ src/day2/ByeWorld.java
@@ -4,5 +4,6 @@ public class ByeWorld {
     public static void main(String[] args) {
         System.out.println("Bye");
         System.out.println("update");
+        System.out.println("update");
     }
 }
